b0b7760c817ad92ac66672ecd8cd0250efffe713
benny <benpakmnz@gmail.com>
2024-05-09 14:34:34 +0300

    ....

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import AppSnackbar from "./components/AppSnackbar";
 import ReactGA from "react-ga4";
 
 const App: React.FC = () => {
-  ReactGA.initialize("G-ZZVCFLKC22");
+  ReactGA.initialize("G-HXH7F2MY82");
   return (
     <StoreContextProvider>
       <div className="app-container">
